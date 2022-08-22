c43bf3cb50223dfee1359f770a7ba3b9fca210cb
Yuri Peres <yuriperes92@gmail.com>
2022-08-22 10:37:21 -0300

    adicionado teste para cadastrar no service, retornando obj com sucesso

--- src/test/java/br/com/springz/service/FuncionarioServiceTest.java
+++ src/test/java/br/com/springz/service/FuncionarioServiceTest.java
@@ -2,14 +2,18 @@ package br.com.springz.service;
 
 import br.com.springz.config.exceptions.ExceptionIdNaoEcontrado;
 import br.com.springz.controller.FuncionarioController;
+import br.com.springz.dtoform.EnderecoForm;
 import br.com.springz.dtoform.FuncionarioDto;
 import br.com.springz.dtoform.FuncionarioDtoDetalhado;
+import br.com.springz.dtoform.FuncionarioForm;
 import br.com.springz.model.Funcionario;
 import br.com.springz.repository.FuncionarioRepository;
 import br.com.springz.utils.EnderecoStub;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -42,6 +46,9 @@ public class FuncionarioServiceTest {
 
     @Mock
     private FuncionarioRepository funcionarioRepository;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     //private FuncionarioForm funcionarioForm = new FuncionarioForm(NOME_VALIDO,SOBRENOME_VALIDO,EMAIL_VALIDO,IDADE_VALIDA,EnderecoStub.ENDERECO_VALIDO);
 
@@ -101,28 +108,41 @@ public class FuncionarioServiceTest {
 
     @Test
     @DisplayName("Verifica se o funcionarioService.acharPorId() lida corretamente com exception")
-    public void acharPorIdComException() {
+    public void acharPorIdComException() throws Exception {
         given(funcionarioRepository.findById(anyLong()))
                 .willThrow(new ExceptionIdNaoEcontrado(OBJETO_NAO_ENCONTRADO));
-        try{
-            funcionarioService.acharPorId(ID_VALIDO);
-        } catch (Exception ex){
-            assertEquals(ExceptionIdNaoEcontrado.class, ex.getClass());
-            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
-        }
+
+        expectedException.expectMessage(OBJETO_NAO_ENCONTRADO);
+        funcionarioService.acharPorId(ID_VALIDO);
+//        try{
+//            funcionarioService.acharPorId(ID_VALIDO);
+//        } catch (Exception ex){
+//            assertEquals(ExceptionIdNaoEcontrado.class, ex.getClass());
+//            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+//        }
 
     }
 
 
     @Test
+    @DisplayName("Quando cadastrar retorna sucesso.")
     public void cadastrarValido() {
-        Funcionario funcionario = new Funcionario(1L,
-                "exemplo de nome","ex de sobrenome", "exde@email.com",30,
-                true, EnderecoStub.ENDERECO_VALIDO);
-        given(funcionarioRepository.save(any(Funcionario.class)))
-                .willAnswer(funcionarioSalvo -> funcionarioSalvo.getArguments()[0]);
-        //assertEquals();
+        given(funcionarioRepository.save(any())).willReturn(FUNCIONARIO_VALIDO);
+        EnderecoForm enderecoform = new EnderecoForm(EnderecoStub.LOCALIDADE_VALIDA, EnderecoStub.LOGRADOURO_VALIDO,
+                EnderecoStub.CEP_VALIDO, EnderecoStub.COMPLEMENTO_VALIDO, EnderecoStub.BAIRRO_VALIDO, EnderecoStub.UF_VALIDA);
 
+        FuncionarioForm form = new FuncionarioForm(NOME_VALIDO, SOBRENOME_VALIDO, EMAIL_VALIDO, IDADE_VALIDA, enderecoform);
+        Funcionario resposta = funcionarioService.cadastrar(form);
+
+        assertNotNull(resposta);
+        assertEquals(Funcionario.class, resposta.getClass());
+        assertEquals(ID_VALIDO, resposta.getId());
+        assertEquals(NOME_VALIDO, resposta.getNome());
+        assertEquals(SOBRENOME_VALIDO, resposta.getSobrenome());
+        assertEquals(EMAIL_VALIDO, resposta.getEmail());
+        assertEquals(IDADE_VALIDA, resposta.getIdade());
+        assertEquals(ATIVO_VALIDO, resposta.getAtivo());
+        assertEquals(EnderecoStub.ENDERECO_VALIDO, resposta.getEndereco());
     }
 
     @Test
